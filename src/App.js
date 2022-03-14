c30b3215ea6d0c7bfb7a2e937f5ee562d8dd6bca
JostesMarcel <jostes77@googlemail.com>
2022-03-14 13:40:16 +0100

    add img

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/header';
+import Sidebar from './Components/sidebar';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Sidebar />
     </div>
   );
 }
---
+++ src/Components/sidebar.jsx
@@ -0,0 +1,17 @@
+export default function Sidebar (){
+    return(
+        <aside>
+            <section class="burger">
+                <span></span>
+            </section>
+
+            <section class="menu">
+            <ul>
+                <li><a href=""><div><img src="" alt="" /></div>Home</a></li>
+                <li><a href=""><div><img src="" alt="" /></div>All Games</a></li>
+                <li><a href=""><div><img src="" alt="" /></div>Recently Added</a></li>
+            </ul>
+            </section>
+        </aside>
+    )
+}
