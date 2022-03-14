e78318efdefdbb886ae3be0a4b1d24143d74e189
JostesMarcel <jostes77@googlemail.com>
2022-03-14 15:04:13 +0100

    aside2

--- src/Components/sidebar.jsx
+++ src/Components/sidebar.jsx
@@ -1,15 +1,28 @@
+import HomeBtn from "../img/HomeBtn.png"
+import AllGame from "../img/AllGame.png"
+import RecentlyKreuz from "../img/RecentlyKreuz.png"
+import "../Components/sidebar.css"
+import { useState } from "react"
+
 export default function Sidebar (){
+const [navbarOpen, setNavbarOpen] = useState(true)
+
+const handleToggle = (prev) => {
+    setNavbarOpen(prev => !prev)
+}
+
     return(
         <aside>
-            <section class="burger">
+            <section onClick={handleToggle} className={navbarOpen ? "burger" : "burger close"} >
+            
                 <span></span>
             </section>
 
             <section class="menu">
             <ul>
-                <li><a href=""><div><img src="" alt="" /></div>Home</a></li>
-                <li><a href=""><div><img src="" alt="" /></div>All Games</a></li>
-                <li><a href=""><div><img src="" alt="" /></div>Recently Added</a></li>
+                <li><a href=""><div><img src={HomeBtn} alt="" /></div>Home</a></li>
+                <li><a href=""><div><img src={AllGame} alt="" /></div>All Games</a></li>
+                <li><a href=""><div><img src={RecentlyKreuz} alt="" /></div>Recently Added</a></li>
             </ul>
             </section>
         </aside>
