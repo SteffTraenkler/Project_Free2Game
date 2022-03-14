6187e6e8e938e987b45fee8caa494d14396eed2a
SuperCoder <supercoder@MacBook-Air-von-SuperCoder-2.local>
2022-03-14 16:49:25 +0100

    CSS fail

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -7,7 +7,6 @@ import { Link } from "react-router-dom";
 export default function Header() {
     return (
         <header>
-            <Link to='/'><div className="logo"><img src={Logo} alt="" /></div></Link>
             <Searchbar />
         </header>
     )
--- src/Components/sidebar.jsx
+++ src/Components/sidebar.jsx
@@ -3,33 +3,34 @@ import AllGame from "../img/AllGame.png"
 import RecentlyKreuz from "../img/RecentlyKreuz.png"
 import "../Components/sidebar.css"
 import { useState } from "react"
+import Logo from '../img/LogoFree2game.png'
+import Searchbar from './searchbar'
+import { Link } from "react-router-dom";
 
-export default function Sidebar (){
-const [navbarOpen, setNavbarOpen] = useState(true)
+export default function Sidebar() {
+    const [navbarOpen, setNavbarOpen] = useState(true)
 
-const handleToggle = (prev) => {
-    setNavbarOpen(prev => !prev)
-}
+    const handleToggle = (prev) => {
+        setNavbarOpen(prev => !prev)
+    }
 
-    return(
-        <aside>
-            <section onClick={handleToggle} className={navbarOpen ? "burger" : "burger close"} >
-            
-                <span></span>
-            </section>
+    return (
+        <div>
+            <Link to='/'><div className={`menuNav ${navbarOpen ? "logoClose" : "logoOpen"}`}><img src={Logo} alt="" /></div></Link>
+            <aside className={`menuNav ${navbarOpen ? " " : "showMenu"}`}>
+                <section onClick={handleToggle} className={navbarOpen ? "burger" : "burger close"} >
 
-            <section class="menu">
-            <ul>
-                <li><a href=""><div><img src={HomeBtn} alt="" /></div>Home</a></li>
-                <li><a href=""><div><img src={AllGame} alt="" /></div>All Games</a></li>
-                <li><a href=""><div><img src={RecentlyKreuz} alt="" /></div>Recently Added</a></li>
-            </ul>
-            <ul>
-                <li><a href=""><div><img src={HomeBtn} alt="" /></div>Home</a></li>
-                <li><a href=""><div><img src={AllGame} alt="" /></div>All Games</a></li>
-                <li><a href=""><div><img src={RecentlyKreuz} alt="" /></div>Recently Added</a></li>
-            </ul>
-            </section>
-        </aside>
+                    <span></span>
+                </section>
+
+                <section>
+                    <ul>
+                        <li><a href=""><div><img src={HomeBtn} alt="" /></div><span className={`menuNav ${navbarOpen ? "hideText" : "showText"}`}>Home</span></a></li>
+                        <li><a href=""><div><img src={AllGame} alt="" /></div><span className={`menuNav ${navbarOpen ? "hideText" : "showText"}`}>All Games</span></a></li>
+                        <li><a href=""><div><img src={RecentlyKreuz} alt="" /></div><span className={`menuNav ${navbarOpen ? "hideText" : "showText"}`}>Recently Added</span></a></li>
+                    </ul>
+                </section>
+            </aside>
+        </div>
     )
 }
