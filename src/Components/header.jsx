52a6eeb1928ce2be620ed6c4a59bb671c24d625c
SuperCoder <supercoder@MacBook-Air-von-SuperCoder-2.local>
2022-03-14 12:00:42 +0100

    Link Logo

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,11 +1,13 @@
 import Logo from '../img/LogoFree2game.png'
 import Searchbar from './searchbar'
 import '../Components/header.css'
+import { Link } from "react-router-dom";
+
 
 export default function Header() {
     return (
         <header>
-            <div className="logo"><img src={Logo} alt="" /></div>
+            <Link to='/'><div className="logo"><img src={Logo} alt="" /></div></Link>
             <Searchbar />
         </header>
     )
--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
