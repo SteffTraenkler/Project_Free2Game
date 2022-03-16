33ef3ef796fa991f549aab47528059b2ecd5105a
JostesMarcel <jostes77@googlemail.com>
2022-03-16 11:34:13 +0100

    Added details.css

--- src/Components/details.jsx
+++ src/Components/details.jsx
@@ -1,6 +1,10 @@
 import { useEffect } from "react"
 import { useState } from "react"
 import { useParams } from "react-router-dom"
+import "../Components/details.css"
+import Header from "./header"
+import Searchbar from "./searchbar"
+import Sidebar from "./sidebar"
 
 export default function Details() {
     let gameID = useParams('id')
@@ -21,36 +25,50 @@ export default function Details() {
 
     return (
         <main className="detailMain">
+            <Header />
+            <Searchbar />
+            <Sidebar />
             <section className="detailHero">
                 <div><img src={screenShot[0].image} alt='Hero Pic of `${gameDetail.title}`' /></div>
             </section>
             <section className="articleDetail">
                 <article className="artGameDescription">
-                    <div>
+                    <div className="gameDetail">
                         <h2>{gameDetail.title}</h2>
                         <div><img src={gameDetail.thumbnail} alt="`Thumbnail of ${gameDetail.title}`" /></div>
-                        <h3>{gameDetail.platform}</h3>
-                        <p className="genreDetail">{gameDetail.genre}</p>
+                        <h3>Platform: {gameDetail.platform}</h3>
+                        <div className="genreDetailDiv">
+                            <p className="genreDetail">{gameDetail.genre}</p>
+                        </div>
                         <a href={gameDetail.game_url}>PLAY NOW</a>
                     </div>
-                    <div>
+                    <div className="aboutSec">
                         <h2>About</h2>
                         <p className='detailDescription'>{gameDetail.description}</p>
                     </div>
                 </article>
                 <article className="artGameDetails">
-                    <div><img src={screenShot[1].image} alt="`Screenshot ${gameDetail.title}`" /></div>
-                    <div><img src={screenShot[2].image} alt="`Screenshot 2 ${gameDetail.title}`" /></div>
+                    <div className="detailsImgFlex">
+                        <div><img src={screenShot[1].image} alt="`Screenshot ${gameDetail.title}`" /></div>
+                        <div><img src={screenShot[2].image} alt="`Screenshot 2 ${gameDetail.title}`" /></div>
+                    </div>
+                    <section className="lastFlex">
                     <article>
                         <h3>Additional Information</h3>
                         <p>Please not this free-to-play game may or may not offer optional in-game purchases</p>
                         <div>
+                            <div className="flex1">
                             <h3>Developer</h3>
                             <p className="dev">{gameDetail.developer}</p>
+                            </div>
+                            <div className="flex2">
                             <h3>Publisher</h3>
                             <p className="publisher">{gameDetail.publisher}</p>
+                            </div>
+                            <div className="flex3">
                             <h3>Release Date</h3>
                             <p className="releaseDate">{gameDetail.release_date}</p>
+                            </div>
                         </div>
 
                     </article>
@@ -58,9 +76,9 @@ export default function Details() {
                         {(() => {
                             if (gameDetail.platform === "Windows") {
                                 return (
-                                    <article>
+                                    <article className="system">
                                         <h2>Minimum System Requirements (Windows)</h2>
-                                        <div>
+                                        <div className="grid">
                                             <div>
                                                 <h3>OS</h3>
                                                 <p>{gameDetail.minimum_system_requirements.os}</p>
@@ -95,6 +113,7 @@ export default function Details() {
                             }
                         })()}
                     </article>
+                    </section>
                 </article>
             </section>
         </main>
